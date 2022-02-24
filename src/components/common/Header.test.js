950f1c1b4ae8f4d2ff2a757ef164770004f0dde3
Naveed Sheikh <nsheikh@lunavi.com>
2022-02-24 13:28:05 -0500

    speech recognition feature added

--- src/components/common/Header.test.js
+++ src/components/common/Header.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../Header";
+import Header from "./Header";
 
 it("should render same text passed into title prop", async () => {
   render(<Header title="My Header" />);
